5f41eebdaa1c83ed5e022172b43c8205fdeb6db7
kee711 <59683761+kee711@users.noreply.github.com>
2025-05-14 03:30:40 +0900

    Delete cohort-learning directory

--- cohort-learning/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '@/types/supabase'
-
-const supabaseUrl = 'https://amhluyzcttjogagmlzce.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtaGx1eXpjdHRqb2dhZ21semNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNTI4NjUsImV4cCI6MjA2MjcyODg2NX0.JbjvolvTn_Yq-t94XFJpxpeU69QM2PhWlNw2xm6v-Io'
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey) 
